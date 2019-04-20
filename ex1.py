328568c587be670aa5d7ddb4af67438b49d3d452
Steven Lai <laisteve17@gmail.com>
2019-04-20 10:46:03 -0700

    Added exercise 1 script and gitignore to ignore Pycharm files

---
+++ ex1.py
@@ -0,0 +1,19 @@
+print("Hello, world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study drills
+
+# 1. Make your script print another line
+print("Another line")
+
+# 2. Make your script print only one of the lines
+print("Imagine I'm printing just one line from the beginning of the script.")
+
+
+# 3. Figure out what the comment character does
+# Octothorpe (#) makes a comment
