090e5c8183ef9263be5b37cc8b0ff73c3c4bb5fe
Steven Lai <laisteve17@gmail.com>
2019-04-20 10:58:36 -0700

    Added exercise 2 script

---
+++ ex2.py
@@ -0,0 +1,39 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+
+# Study Drills
+
+
+# 1. Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or
+# pound character).
+#
+# It was a comment symbol that I'll call "pound"
+
+
+# 2. Take your ex2.py file and review each line going backward. Start at the last line, and check each word in reverse
+# against what you should have typed.
+#
+# Okay, looks good!
+
+
+# 3. Did you find more mistakes? Fix them.
+#
+# Nope, no mistakes. Made sure to type character by character like the book says to do even thought I felt like I
+# already knew how to comment stuff out. Trying to build the discipline to do stuff that I don't want to do for the sake
+# of learning to learn effectively. I was introduced to reading stuff backwards to check for errors by my ISE professor.
+# It was super effective.
+
+
+# 4. Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix
+# them.
+#
+# Okay. Interesting drill. Made me realize how hard it is to "speak" what I'm typing. Might help in pair programming
+# when we try to tell each other what to type.
