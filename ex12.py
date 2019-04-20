eb45685d47750f24d555ebe6dfcd394b50ede8cf
Steven Lai <laisteve17@gmail.com>
2019-04-20 12:42:28 -0700

    Added exercise 12 script

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# Prompt string accepted in input function
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
+
+# Can run 'pydoc input' in TERMINAL (not in IDLE) to get docs for some funciton.
+# Press q to quit
