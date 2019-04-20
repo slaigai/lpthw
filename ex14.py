3f9059bb3549d94fa00e9ee7ad2820a628cc1dbe
Steven Lai <laisteve17@gmail.com>
2019-04-20 12:57:31 -0700

    Added exercise 14 script

---
+++ ex14.py
@@ -0,0 +1,24 @@
+from sys import argv
+
+script, user_name = argv
+prompt = '> '  # prevent typing this over and over again
+
+print(f"Hi {user_name}, I'm the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+# Notice ability to create multi-line f-strings
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
+
+# Note multi-line """{}""".format(arg) works too
