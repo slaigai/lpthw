a8a4f91a5def7028cc05f7f2d3a038a5adce034a
Steven Lai <laisteve17@gmail.com>
2019-04-20 18:19:45 -0700

    Added exercise 29 script

---
+++ ex29.py
@@ -0,0 +1,39 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
+
+# Study Drills
+
+# 1. the if keyword will only run the code indented under it if the condition is True
+
+# 2. To indicate that it is to be run if the condition is met. It's just python syntax...
+
+# 3. If the code under the if is not indented, it runs unconditionally.
+
+# 4. Nah
+
+# 5. It prints out different things depending on what values you give it...
